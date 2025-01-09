2b19ba547024a2826a4aacb688cca99e7ec272ea
lehojun <hojunchris@naver.com>
2025-01-10 00:35:25 +0900

    [FEAT]dev_deploy.yml 파일 설정

---
+++ spring/src/main/java/hackathon/spring/web/controller/TestController.java
@@ -0,0 +1,19 @@
+package hackathon.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
